d79eed04c4b875e861e5e839de39e8fb9a8d9d46
dbulan <dimitar.bulanov@gmail.com>
2021-11-11 12:29:30 +0200

    next

--- mssql.cs
+++ mssql.cs
@@ -1,3 +1,19 @@
 # Tips & Tricks
 
-getdate() // current datetime
+# Date
+
+getdate() 			// current datetime
+MONTH(getdate()) 	// month number
+
+# Create Table with PK
+
+CREATE TABLE [table] (
+    id int NOT NULL,
+    name varchar(255) NOT NULL, 			// no-default
+	points float NOT NULL DEFAULT ((0.00)),	// is-default
+    PRIMARY KEY (id)
+);
+
+# Alter Table
+
+ALTER TABLE [table] ADD [column] int NOT NULL DEFAULT((0))
