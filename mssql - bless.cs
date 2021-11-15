0289eb89dfc5ec0c1ee71e0a6e87085ca2b6e951
dbulan <dimitar.bulanov@gmail.com>
2021-11-15 16:41:10 +0200

    search in hex

--- mssql - bless.cs
+++ mssql - bless.cs
@@ -1,3 +1,11 @@
 # Delete VIP bless
 
 UPDATE [MEMB_INFO] SET SCFIsVip = 0, SCFVipTill = getdate() WHERE [memb___id] = '{AccountID}'
+
+# Search in HEX
+
+// Search by serial in HEX
+SELECT * FROM table WHERE CONV(SUBSTRING(`hex`, 7, 8), 16, 10) = '%serial')
+
+// Search by type and id in HEX
+SELECT * FROM table WHERE CONV(SUBSTRING(`hex`, 19, 1), 16, 10) = 12 AND CONV(SUBSTRING(`hex`, 1, 2), 16, 10) = 201
